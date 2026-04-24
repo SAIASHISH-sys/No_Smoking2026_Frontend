d7eabfc8840c81a75f7530f8d2cbd254b4b9f28b
Sai Ashish Mishra <saiashishmishra290806@gmail.com>
2026-04-25 04:30:07 +0530

    shifted all the endpoints to env

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import { getAuthToken } from './utils/authUtils';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4002/graphql',
+  uri: import.meta.env.VITE_API_URL,
   credentials: 'include', // send httpOnly cookies for auth
 });
 
